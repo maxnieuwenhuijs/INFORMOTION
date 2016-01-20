1b6489fe262f76603175b67510173a7df3745f24
maxnieuwenhuijs <max.nieuwenhuijs@gmail.com>
2016-01-20 19:09:12 +0100

    4

---
+++ js/data.js
@@ -0,0 +1,49 @@
+var contentData = '{"vuilnisbakData":[' +
+'{"Serial":100864,"Maandag":138,"Dinsdag":38,"Woensdag":125,"Donderdag":31,"Vrijdag":38,"Zaterdag":"-","Zondag":"-","Adres":"Blokmakerstraat","Huisnummer":39},'+
+'{"Serial":100861,"Maandag":129,"Dinsdag":44,"Woensdag":138,"Donderdag":31,"Vrijdag":81,"Zaterdag":"6","Zondag":"50","Adres":"Bickersgracht","Huisnummer":260},'+
+'{"Serial":100865,"Maandag":88,"Dinsdag":81,"Woensdag":75,"Donderdag":75,"Vrijdag":38,"Zaterdag":"25","Zondag":"75","Adres":"De Ruijterkade","Huisnummer":6},'+
+'{"Serial":100859,"Maandag":119,"Dinsdag":31,"Woensdag":63,"Donderdag":13,"Vrijdag":38,"Zaterdag":"-","Zondag":"-","Adres":"Bickersgracht","Huisnummer":46},'+
+'{"Serial":100889,"Maandag":88,"Dinsdag":75,"Woensdag":75,"Donderdag":75,"Vrijdag":44,"Zaterdag":"13","Zondag":"38","Adres":"Planciusstraat","Huisnummer":25},'+
+'{"Serial":100866,"Maandag":88,"Dinsdag":38,"Woensdag":75,"Donderdag":63,"Vrijdag":38,"Zaterdag":"13","Zondag":"-","Adres":"De Ruijterkade","Huisnummer":15},'+
+'{"Serial":100869,"Maandag":88,"Dinsdag":13,"Woensdag":75,"Donderdag":19,"Vrijdag":13,"Zaterdag":"-","Zondag":"-","Adres":"Grote Bickerstraat","Huisnummer":86},'+
+'{"Serial":100893,"Maandag":88,"Dinsdag":56,"Woensdag":69,"Donderdag":54,"Vrijdag":19,"Zaterdag":"-","Zondag":"-","Adres":"Prinseneiland","Huisnummer":69},'+
+'{"Serial":100879,"Maandag":88,"Dinsdag":44,"Woensdag":69,"Donderdag":31,"Vrijdag":19,"Zaterdag":"-","Zondag":"25","Adres":"Hollandse tuin","Huisnummer":16},'+
+'{"Serial":100903,"Maandag":81,"Dinsdag":63,"Woensdag":75,"Donderdag":75,"Vrijdag":44,"Zaterdag":"25","Zondag":"75","Adres":"Westerdok","Huisnummer":272},'+
+'{"Serial":100888,"Maandag":81,"Dinsdag":56,"Woensdag":75,"Donderdag":75,"Vrijdag":44,"Zaterdag":"19","Zondag":"50","Adres":"Nieuwe Teertuinen","Huisnummer":60},'+
+'{"Serial":100881,"Maandag":81,"Dinsdag":31,"Woensdag":75,"Donderdag":31,"Vrijdag":44,"Zaterdag":"13","Zondag":"19","Adres":"Houtmanstraat","Huisnummer":39},'+
+'{"Serial":100894,"Maandag":81,"Dinsdag":50,"Woensdag":56,"Donderdag":38,"Vrijdag":19,"Zaterdag":"-","Zondag":"-","Adres":"Prinseneiland","Huisnummer":9999},'+
+'{"Serial":100908,"Maandag":81,"Dinsdag":44,"Woensdag":50,"Donderdag":19,"Vrijdag":31,"Zaterdag":"28","Zondag":"38","Adres":"Zandhoek","Huisnummer":14},'+
+'{"Serial":100873,"Maandag":75,"Dinsdag":94,"Woensdag":75,"Donderdag":81,"Vrijdag":50,"Zaterdag":"63","Zondag":"63","Adres":"Hendrik Jonkerplein","Huisnummer":10},'+
+'{"Serial":100886,"Maandag":75,"Dinsdag":19,"Woensdag":75,"Donderdag":19,"Vrijdag":44,"Zaterdag":"16","Zondag":"13","Adres":"Korte Zoutkeetsgracht","Huisnummer":2},'+
+'{"Serial":100872,"Maandag":75,"Dinsdag":69,"Woensdag":69,"Donderdag":75,"Vrijdag":41,"Zaterdag":"25","Zondag":"69","Adres":"Hendrik Jonkerplein","Huisnummer":2},'+
+'{"Serial":100904,"Maandag":75,"Dinsdag":63,"Woensdag":69,"Donderdag":75,"Vrijdag":44,"Zaterdag":"6","Zondag":"38","Adres":"Westerdok","Huisnummer":606},'+
+'{"Serial":100863,"Maandag":75,"Dinsdag":19,"Woensdag":63,"Donderdag":13,"Vrijdag":19,"Zaterdag":"-","Zondag":"-","Adres":"Blokmakerstraat","Huisnummer":39},'+
+'{"Serial":100895,"Maandag":75,"Dinsdag":44,"Woensdag":62,"Donderdag":0,"Vrijdag":38,"Zaterdag":"-","Zondag":"25","Adres":"Realengracht","Huisnummer":2},'+
+'{"Serial":100898,"Maandag":75,"Dinsdag":38,"Woensdag":56,"Donderdag":56,"Vrijdag":50,"Zaterdag":"13","Zondag":"44","Adres":"Vierwindendwarsstraat","Huisnummer":1},'+
+'{"Serial":100899,"Maandag":75,"Dinsdag":31,"Woensdag":56,"Donderdag":19,"Vrijdag":-,"Zaterdag":"-","Zondag":"-","Adres":"Vierwindenstraat","Huisnummer":201},'+
+'{"Serial":100883,"Maandag":75,"Dinsdag":25,"Woensdag":56,"Donderdag":19,"Vrijdag":56,"Zaterdag":"16","Zondag":"-","Adres":"Jan Mensplein","Huisnummer":42},'+
+'{"Serial":100896,"Maandag":75,"Dinsdag":25,"Woensdag":56,"Donderdag":19,"Vrijdag":38,"Zaterdag":"-","Zondag":"-","Adres":"Schiemanstraat","Huisnummer":9999},'+
+'{"Serial":100882,"Maandag":75,"Dinsdag":19,"Woensdag":50,"Donderdag":38,"Vrijdag":38,"Zaterdag":"-","Zondag":"19","Adres":"Houtmanstraat","Huisnummer":81},'+
+'{"Serial":100868,"Maandag":69,"Dinsdag":13,"Woensdag":88,"Donderdag":19,"Vrijdag":25,"Zaterdag":"31","Zondag":"38","Adres":"Grote Bickerstraat","Huisnummer":86},'+
+'{"Serial":100905,"Maandag":69,"Dinsdag":13,"Woensdag":81,"Donderdag":-,"Vrijdag":44,"Zaterdag":"31","Zondag":"25","Adres":"Westerdok","Huisnummer":816},'+
+'{"Serial":100902,"Maandag":69,"Dinsdag":50,"Woensdag":75,"Donderdag":69,"Vrijdag":38,"Zaterdag":"13","Zondag":"50","Adres":"Westerdok","Huisnummer":49},'+
+'{"Serial":100875,"Maandag":69,"Dinsdag":25,"Woensdag":75,"Donderdag":38,"Vrijdag":25,"Zaterdag":"6","Zondag":"31","Adres":"Hollandse tuin","Huisnummer":5},'+
+'{"Serial":100901,"Maandag":69,"Dinsdag":38,"Woensdag":69,"Donderdag":75,"Vrijdag":44,"Zaterdag":"19","Zondag":"56","Adres":"Westerdok","Huisnummer":1},'+
+'{"Serial":100858,"Maandag":69,"Dinsdag":19,"Woensdag":69,"Donderdag":19,"Vrijdag":43.75,"Zaterdag":"16","Zondag":"-","Adres":"Bickersgracht","Huisnummer":46},'+
+'{"Serial":100878,"Maandag":69,"Dinsdag":19,"Woensdag":69,"Donderdag":38,"Vrijdag":31,"Zaterdag":"28","Zondag":"25","Adres":"Hollandse tuin","Huisnummer":14},'+
+'{"Serial":100874,"Maandag":69,"Dinsdag":56,"Woensdag":63,"Donderdag":69,"Vrijdag":38,"Zaterdag":"31","Zondag":"31","Adres":"Hendrik Jonkerplein","Huisnummer":9999},'+
+'{"Serial":100891,"Maandag":69,"Dinsdag":50,"Woensdag":63,"Donderdag":50,"Vrijdag":-,"Zaterdag":"-","Zondag":"-","Adres":"Prinseneiland","Huisnummer":48},'+
+'{"Serial":100867,"Maandag":69,"Dinsdag":31,"Woensdag":56,"Donderdag":50,"Vrijdag":25,"Zaterdag":"19","Zondag":"13","Adres":"De Ruijterkade","Huisnummer":16},'+
+'{"Serial":100906,"Maandag":69,"Dinsdag":-,"Woensdag":44,"Donderdag":31,"Vrijdag":31,"Zaterdag":"19","Zondag":"31","Adres":"Westerdoksplein","Huisnummer":9999},'+
+'{"Serial":100907,"Maandag":69,"Dinsdag":31,"Woensdag":44,"Donderdag":13,"Vrijdag":50,"Zaterdag":"13","Zondag":"13","Adres":"Zandhoek ","Huisnummer":4},'+
+'{"Serial":100877,"Maandag":69,"Dinsdag":19,"Woensdag":38,"Donderdag":50,"Vrijdag":50,"Zaterdag":"22","Zondag":"19","Adres":"Hollandse tuin","Huisnummer":11},'+
+'{"Serial":100897,"Maandag":63,"Dinsdag":31,"Woensdag":81,"Donderdag":38,"Vrijdag":31,"Zaterdag":"22","Zondag":"25","Adres":"Touwslagerstraat","Huisnummer":51},'+
+'{"Serial":100890,"Maandag":63,"Dinsdag":56,"Woensdag":63,"Donderdag":56,"Vrijdag":19,"Zaterdag":"-","Zondag":"25","Adres":"Prinseneiland","Huisnummer":43},'+
+'{"Serial":100884,"Maandag":56,"Dinsdag":25,"Woensdag":63,"Donderdag":44,"Vrijdag":6,"Zaterdag":"-","Zondag":"-","Adres":"Keerpunt","Huisnummer":12},'+
+'{"Serial":100885,"Maandag":56,"Dinsdag":6,"Woensdag":56,"Donderdag":25,"Vrijdag":44,"Zaterdag":"19","Zondag":"13","Adres":"Korte Zoutkeetsgracht","Huisnummer":2},'+
+'{"Serial":100862,"Maandag":50,"Dinsdag":25,"Woensdag":75,"Donderdag":-,"Vrijdag":13,"Zaterdag":"13","Zondag":"13","Adres":"Bickersgracht","Huisnummer":286},'+
+'{"Serial":100876,"Maandag":50,"Dinsdag":19,"Woensdag":75,"Donderdag":31,"Vrijdag":19,"Zaterdag":"16","Zondag":"13","Adres":"Hollandse tuin","Huisnummer":9},'+
+'{"Serial":100880,"Maandag":50,"Dinsdag":13,"Woensdag":69,"Donderdag":19,"Vrijdag":null,"Zaterdag":"-","Zondag":"-","Adres":"Houtmanstraat","Huisnummer":1},'+
+'{"Serial":100892,"Maandag":44,"Dinsdag":25,"Woensdag":56,"Donderdag":38,"Vrijdag":-,"Zaterdag":"-","Zondag":"-","Adres":"Prinseneiland","Huisnummer":397},'+
+'{"Serial":100887,"Maandag":38,"Dinsdag":69,"Woensdag":88,"Donderdag":69,"Vrijdag":44,"Zaterdag":"19","Zondag":"69","Adres":"Nieuwe Teertuinen","Huisnummer":3},'+
+'{"Serial":100900,"Maandag":31,"Dinsdag":38,"Woensdag":56,"Donderdag":19,"Vrijdag":38,"Zaterdag":"-","Zondag":"-","Adres":"Vierwindenstraat","Huisnummer":201}]}';
